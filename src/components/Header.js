df029e68be20272215abf465bc13e1856c934e09
VinCoD <5895.2020@students.ku.ac.ke>
2022-06-19 11:10:50 +0300

    Header expand full width

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,10 @@ function Header() {
                 <img src="/images/logo.svg"/>
             </a>
             <Menu>
-
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model X</a></p>
+                <p><a href="#">Model Y</a></p>
 
             </Menu>
         </Container>
@@ -19,9 +22,24 @@ const Container = styled.div`
     min-height: 60px;
     position: fixed;
     display: flex;
-    
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
 
 `
 const Menu = styled.div`
-
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    
+    p{
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+        
+    }
 `
